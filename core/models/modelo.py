bbcda4dd613322e7d565f5b4f5ce371908340df0
erickChavesFiamoncini <erickechaves@gmail.com>
2026-03-23 16:45:49 -0300

    feat: Criação da API para modelos

---
+++ core/models/modelo.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Modelo(models.Model):
+    nome = models.CharField(max_length=80)
+    marca = models.CharField(max_length=80, null=True, blank=True)
+    categoria = models.CharField(max_length=80, null=True, blank=True)
+
+    def __str__(self):
+        marca = self.marca.upper() if self.marca else ""
+        nome = self.nome.upper() if self.nome else ""
+        return f'({self.id}) {marca} {nome}'
